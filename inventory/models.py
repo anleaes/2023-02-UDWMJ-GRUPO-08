ebc91a231d739867093e4e5d05ef7465450c965a
Yuri Propp Mendes <yuri200503@gmail.com>
2023-11-26 20:22:33 -0300

    new file:   db.sqlite3 	new file:   inventory/__init__.py 	new file:   inventory/__pycache__/__init__.cpython-39.pyc 	new file:   inventory/__pycache__/models.cpython-39.pyc 	new file:   inventory/__pycache__/settings.cpython-39.pyc 	new file:   inventory/__pycache__/urls.cpython-39.pyc 	new file:   inventory/asgi.py 	new file:   inventory/models.py 	new file:   inventory/settings.py 	new file:   inventory/urls.py 	new file:   inventory/wsgi.py 	new file:   manage.py

---
+++ inventory/models.py
@@ -0,0 +1,20 @@
+# inventory/models.py
+from django.db import models
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    points = models.IntegerField()
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    points = models.IntegerField(default=100)
+
+class Inventory(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+class Adventure(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    points_earned = models.IntegerField()
+    items_lost = models.ManyToManyField(Item)
