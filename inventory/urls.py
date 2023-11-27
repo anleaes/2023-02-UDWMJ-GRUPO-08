04d3465f0512ec4ea8ef6d80373e468f5049b124
Yuri Propp Mendes <yuri200503@gmail.com>
2023-11-27 08:07:55 -0300

    modified:   db.sqlite3 	new file:   inventory/__pycache__/admin.cpython-39.pyc 	modified:   inventory/settings.py 	modified:   inventory/urls.py

--- inventory/urls.py
+++ inventory/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from .views import index, market, adventure, buy_item
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('market/', market, name='market'),
     path('adventure/', adventure, name='adventure'),
     path('buy_item/<int:item_id>/', buy_item, name='buy_item'),
 ]
+
