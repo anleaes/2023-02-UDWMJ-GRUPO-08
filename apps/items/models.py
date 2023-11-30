6bf8cb396a404c035cbdc2717d36a7d221e0c5b2
Yuri Propp Mendes <yuri200503@gmail.com>
2023-11-30 11:07:22 -0300

    Arrumação do items

--- apps/items/models.py
+++ apps/items/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from categories.models import Category
 
 # Create your models here.
+
 class Item(models.Model):
     name = models.CharField('Nome', max_length=50)
     description = models.TextField('Descricao', max_length=100)
--- apps/items/views.py
+++ apps/items/views.py
@@ -19,16 +19,16 @@ def add_item(request):
 
 def list_items(request):
     template_name = 'items/list_items.html'
-    items = Item.objects.filter()
+    item = Item.objects.filter()
     context = {
-        'items': items
+        'items': item
     }
     return render(request, template_name, context)
 
 def edit_item(request, id_item):
     template_name = 'items/add_item.html'
     context ={}
-    item = get_object_or_404(item, id=id_item)
+    item = get_object_or_404(Item, id=id_item)
     if request.method == 'POST':
         form = ItemForm(request.POST, request.FILES,  instance=item)
         if form.is_valid():
